7e76f22724b57d75c5905b079c705f65bfda956a
Ryan Allen <ryanallen@users.noreply.github.com>
2025-05-12 16:06:11 -0700

    Add ELI5 comments to db.js

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
-require('dotenv').config();
-const {MongoClient} = require('mongodb');
-module.exports = () => new MongoClient(process.env.MONGO_URI).connect(); 
+require('dotenv').config();  // Load the secret password from the .env file
+
+const {MongoClient} = require('mongodb');  // Get the tool that helps us talk to MongoDB
+
+module.exports = () => new MongoClient(process.env.MONGO_URI).connect();  // Give other files a way to connect to our database 
