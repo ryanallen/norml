a80e97b5173ff5592a4fe1bf74741e208c7ba5aa
Ryan Allen <ryanallen@users.noreply.github.com>
2025-05-14 11:36:10 -0700

    fix headers and db issues

--- ports/headers.js
+++ ports/headers.js
@@ -2,16 +2,19 @@
 export class ResponseHeaders {
   static getDefaultHeaders() {
     return {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json; charset=utf-8',
       'Access-Control-Allow-Origin': '*', // Allow cross-origin requests
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Supported methods
-      'Access-Control-Allow-Headers': 'Content-Type' // Allowed headers
+      'Access-Control-Allow-Headers': 'Content-Type', // Allowed headers
+      'X-Content-Type-Options': 'nosniff', // Prevent MIME type sniffing
+      'Cache-Control': 'no-cache, no-store, must-revalidate', // Prevent caching
+      'Access-Control-Expose-Headers': 'Content-Length, Content-Type' // Expose these headers to the browser
     };
   }
 
   static getHeadersFor(contentType = 'application/json') {
     const headers = this.getDefaultHeaders();
-    headers['Content-Type'] = contentType;
+    headers['Content-Type'] = `${contentType}; charset=utf-8`;
     return headers;
   }
 }
